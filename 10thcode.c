e80b9ef65ba0fecf4ef96f89195cb9dcb87660dc
Vishwanath N Ganiger <130492202+01fe22bec249@users.noreply.github.com>
2024-03-13 00:04:32 +0530

    Add files via upload

---
+++ 10thcode.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    float rec_width;
+    float rec_height;
+    float rec_perimeter;
+
+    printf("Input the height of the Rectangle : ");
+    scanf("%f", &rec_height);
+    printf("Input the width of the Rectangle : ");
+    scanf("%f", &rec_width);
+
+    rec_perimeter = 2.0 * (rec_height + rec_width);
+
+
+    printf("Perimeter of the Rectangle is : %f\n", rec_perimeter);
+
+    return 0;
+
+
